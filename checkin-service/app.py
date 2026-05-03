d4827d149503b17b677e8aa20addcb3f05f7d0d5
Incrediblez7 <william.zhang1093@gmail.com>
2026-05-03 03:50:16 -0400

    chore: port specification, docker-compose unification, readme.

--- checkin-service/app.py
+++ checkin-service/app.py
@@ -58,5 +58,5 @@ def seed_rooms():
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5001))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
